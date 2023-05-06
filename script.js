2e3603304dcbb0b0328380ccbada82c96004bef1
nrtimms <nataliertimms18@gmail.com>
2023-05-06 12:02:53 -0700

    added water backgroud

--- script.js
+++ script.js
@@ -1,148 +1,59 @@
-// function newNonPlayableCharacter(x, y) {
-//     let element = document.getElementById("circle")
-//     element.style.zIndex = 1;
-//     let direction = null;
+// const circleObject = {
+//     circle: document.getElementById("circle"),
+//     x: 9,
+//     y: 17,
+//     goLeft: false,
+//     count: 5,
+// }
 
-//     function moveCharacter() {
-//         if (direction === 'west') {
-//             x -= 1
+// function moveCircle(circleObject){
+//     circleObject.circle.style.left = circleObject.x + "px";
+//     circleObject.circle.style.top = circleObject.y + "px";
+//         if (circleObject.goLeft == false){
+//             circleObject.circle.style.left = circleObject.x + 3 + "px";
+//             circleObject.x = circleObject.x + 3;
 //         }
-//         if (direction === 'east') {
-//             x += 1
+//         if (circleObject.goLeft == true){
+//             circleObject.circle.style.left = circleObject.x - 3 + "px";
+//             circleObject.x = circleObject.x - 3;
 //         }
-//         element.style.left = x + 'px'
-//         element.style.bottom = y + 'px'
+//         if(circleObject.x>380){
+//             circleObject.goLeft=true;
+//         }
+//         if(circleObject.x<10){
+//             circleObject.goLeft=false;
+//         }
+// }
+// window.setInterval(moveCircle, 10, circleObject)
+
+// function clickclick(circleObject){
+//     if(circleObject.x>150 && circleObject.x<250){
+//         circleObject.count ++
 //     }
-
-//     setInterval(moveCharacter, 1)
-
-//     async function walkEast(time) {
-//         direction = 'east'
-//         await sleep(time)
-//         stop()
+//     if(circleObject.x<150 || circleObject.x>250){
+//         circleObject.count --
 //     }
-
-//     async function walkWest(time) {
-//         direction = 'west'
-//         await sleep(time)
-//         stop()
+//     if(circleObject.count == 10){
+//         console.log("win")
 //     }
-
-//     function stop() {
-//         direction = null
-//     }
-
-//     function sleep(time){
-//         return new Promise(resolve => {
-//             setTimeout(resolve, time)
-//         })  
-//     }
-
-
-//     return {
-//         element: element,
-//         walkWest: walkWest,
-//         walkEast: walkEast,
-//         stop: stop
+//     if(circleObject.count == 4){
+//         console.log("loss")
 //     }
 // }
 
-// const npc = newNonPlayableCharacter(0, 0)
+// document.getElementById("rect").onclick = function() {clickclick(circleObject)};
 
-// have the NPC start walking east immediately
-// let i = 0
-// async function moveNPC(){
-//     while(i<5){
-//         await npc.walkWest(1400)
-//         await npc.walkEast(1200)
-//         i++
-//     }
-// }
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-// moveNPC()
+canvas.width = 1024
+canvas.height = 576
 
-// let circle = document.getElementById("circle")
-// var x = 9;
-// var y = 17;
-// circle.style.left = x + "px";
-// circle.style.top = y + "px";
+c.fillRect(0, 0, canvas.width, canvas.height)
 
-// // // // let x = 40;
-// // // // let y = 40;
-// let goLeft = false;
-// // // // function setUp(){
-// // // //     createCanvas(400, 400);
-// // // // }
+const image = new Image()
+image.src = '/assets/map.png'
 
-// function moveCircle(x){
-//         if (goLeft == false){
-//             circle.style.left = x + 3 + "px";
-//             x = x + 3;
-//         }
-//         if (goLeft == true){
-//             circle.style.left = x - 3 + "px";
-//             x = x - 3;
-//         }
-//         if(x>400){
-//             goLeft=true;
-//         }
-//         if(x<0){
-//             goLeft=false;
-//         }
-//         return x
-// }
-// // var x = moveCircle(x)
-// // console.log(x)
-
-// setInterval(moveCircle(moveCircle()), 10)
-
-//var interval = setInterval(function() {
-//     var div = document.getElementById("circle");
-//     var goLeft = false;
-//     var x = 0;
-//     if(goLeft == false){
-//         div.style.left = div.offsetLeft + 3 + "px";
-//         x = x + 3;
-//     }
-//     if(goLeft == true){
-//         div.style.left = div.offsetLeft - 3 + "px";
-//         x = x - 3;
-//     }
-//     if(x>400){
-//         goLeft = true;
-//     }
-//     if(x<0){
-//         goLeft = false;
-//     }
-// },10);
-
-const circleObject = {
-    circle: document.getElementById("circle"),
-    x: 9,
-    y: 17,
-    goLeft: false,
+image.onload = () => {
+    c.drawImage(image, 0, 0)
 }
-
-function moveCircle(circleObject){
-    circleObject.circle.style.left = circleObject.x + "px";
-    circleObject.circle.style.top = circleObject.y + "px";
-        if (circleObject.goLeft == false){
-            circleObject.circle.style.left = circleObject.x + 3 + "px";
-            circleObject.x = circleObject.x + 3;
-        }
-        if (circleObject.goLeft == true){
-            circleObject.circle.style.left = circleObject.x - 3 + "px";
-            circleObject.x = circleObject.x - 3;
-        }
-        if(circleObject.x>380){
-            circleObject.goLeft=true;
-        }
-        if(circleObject.x<10){
-            circleObject.goLeft=false;
-        }
-}
-
-window.setInterval(moveCircle, 10, circleObject)
-
-//console.log(circleObject.x)
-
