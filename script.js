cce91105546905072e157848cc1155eb4f456e91
nrtimms <nataliertimms18@gmail.com>
2023-05-08 20:34:25 -0700

    collision detection and marked fish spots

--- script.js
+++ script.js
@@ -51,16 +51,83 @@ canvas.height = 576
 
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+class Boundary {
+    static width = 128
+    static height = 128
+    constructor({position}) {
+        this.position = position
+        this.width = 128
+        this.height = 128
+    }
+    draw() {
+        c.fillStyle = 'rgba(255, 0, 0, 0.2)'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const boundaries = []
+
+collisionsMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 3)
+        boundaries.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width,
+                    y: i * Boundary.height
+                }
+            })
+        )
+    })
+})
+
+const fish = []
+
+fishiesMap.forEach((row, i) => {
+    row.forEach((symbol, j) => {
+        if (symbol === 1)
+        fish.push(
+            new Boundary({
+                position: {
+                    x: j * Boundary.width,
+                    y: i * Boundary.height
+                }
+            })
+        )
+    })
+})
+
 const image = new Image()
 image.src = '/assets/map.png'
 
+const playerImage = new Image()
+playerImage.src = '/assets/boat.png'
+
 class Sprite {
-    constructor({position, velocity, image}) {
+    constructor({position, image}) {
         this.position = position
         this.image = image
     }
     draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
+        c.drawImage(
+            this.image, 
+            this.position.x, 
+            this.position.y,
+        )
+    }
+}
+
+class Player {
+    static width = 192
+    static height = 192
+    constructor({position, image}) {
+        this.position = position
+        this.image = image
+        this.width = 192
+        this.height = 192
+    }
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y, this.width, this.height)
     }
 }
 
@@ -69,7 +136,15 @@ const background = new Sprite({
         x: 0,
         y: 0
     },
-    image: image
+    image: image,
+})
+
+const boat = new Player({
+    position: {
+        x: 400,
+        y: 200
+    },
+    image: playerImage,
 })
 
 const keys = {
@@ -87,14 +162,123 @@ const keys = {
     }
 }
 
+const movables = [background, ...boundaries, ...fish]
+
+function rectangularCollision({thing1, thing2}) {
+    return (
+        thing1.position.x + thing1.width -20 >= thing2.position.x &&
+        thing1.position.x <= thing2.position.x + thing2.width - 20 &&
+        thing1.position.y + thing1.height - 50 >= thing2.position.y &&
+        thing1.position.y <= thing2.position.y + thing2.height -20
+    )
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     background.draw()
+    boat.draw()
+    
+    boundaries.forEach(boundary => {
+        boundary.draw()
+    })
+    fish.forEach(fish => {
+        fish.draw()
+    })
 
-    if(keys.w.pressed) background.position.y += 3
-    else if(keys.a.pressed) background.position.x += 3
-    else if(keys.s.pressed) background.position.y -= 3
-    else if(keys.d.pressed) background.position.x -= 3
+    let moving = true
+    if(keys.w.pressed) {
+        for (let i=0; i<boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    thing1: boat,
+                    thing2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x,
+                            y: boundary.position.y + 3
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+            movables.forEach((movable) => {
+                movable.position.y += 3
+            })
+    } else if(keys.a.pressed) {
+        for (let i=0; i<boundaries.length; i++) {
+            const boundary = boundaries[i]
+            if (
+                rectangularCollision({
+                    thing1: boat,
+                    thing2: {
+                        ...boundary,
+                        position: {
+                            x: boundary.position.x + 3,
+                            y: boundary.position.y
+                        }
+                    }
+                })
+            ) {
+                moving = false
+                break
+            }
+        }
+        if (moving)
+            movables.forEach((movable) => {
+                movable.position.x += 3
+            })
+    } else if(keys.s.pressed) {
+            for (let i=0; i<boundaries.length; i++) {
+                const boundary = boundaries[i]
+                if (
+                    rectangularCollision({
+                        thing1: boat,
+                        thing2: {
+                            ...boundary,
+                            position: {
+                                x: boundary.position.x,
+                                y: boundary.position.y - 3
+                            }
+                        }
+                    })
+                ) {
+                    moving = false
+                    break
+                }
+            }
+            if (moving)
+                movables.forEach((movable) => {
+                    movable.position.y -= 3
+                })
+        }else if(keys.d.pressed) {
+            for (let i=0; i<boundaries.length; i++) {
+                const boundary = boundaries[i]
+                if (
+                    rectangularCollision({
+                        thing1: boat,
+                        thing2: {
+                            ...boundary,
+                            position: {
+                                x: boundary.position.x - 10,
+                                y: boundary.position.y
+                            }
+                        }
+                    })
+                ) {
+                    moving = false
+                    break
+                }
+            }
+            if (moving)
+                movables.forEach((movable) => {
+                    movable.position.x -= 3
+                })
+        }
 }
 animate()
 
